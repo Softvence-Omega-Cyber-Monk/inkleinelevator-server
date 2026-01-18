5294c13e10072a01e3e515c55fef64977cc5d39a
mahdijihad001 <mahdijihad001@gmail.com>
2026-01-18 17:18:38 +0600

    Fix Another Enum Types

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { User } from 'generated/prisma/client';
+import { Prisma , User} from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
